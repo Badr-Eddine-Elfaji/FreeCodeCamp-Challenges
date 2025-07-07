22dce991cf69eb85a3eec72e81e699a47404c1bb
Badr-Eddine-Elfaji <143713102+Badr-Eddine-Elfaji@users.noreply.github.com>
2025-07-07 14:02:54 +0100

    Update mean_var_std.py

--- Mean-Var-Std/mean_var_std.py
+++ Mean-Var-Std/mean_var_std.py
@@ -1,44 +1,44 @@
 import numpy as np
 
-def calculate(list) :
+def calculate(list):
     if len(list) != 9 :
         raise ValueError("List must contain nine numbers.")
     
     #Convert list into a numpy array of 3,3
-    matrix = np.array(list).reshape(3, 3)
+    matrix = np.array(list).reshape(3,3)
 
     #Calculate Statistics
     calculations = {
-        "Mean": [
+        "mean": [
             matrix.mean(axis=0).tolist(),    #Columns
             matrix.mean(axis=1).tolist(),    #Rows
             matrix.mean().tolist(),          #Flattened
         ],
-        "Variance": [
+        "variance": [
             matrix.var(axis=0).tolist(),
             matrix.var(axis=1).tolist(),
             matrix.var().tolist(),
         ],
-        "Standard Deviation": [
+        "standard deviation": [
             matrix.std(axis=0).tolist(),
             matrix.std(axis=1).tolist(),
             matrix.std().tolist(),
         ],
-        "Max": [
+        "max": [
             matrix.max(axis=0).tolist(),
             matrix.max(axis=1).tolist(),
             matrix.max().tolist(),
         ],
-        "Min": [
+        "min": [
             matrix.min(axis=0).tolist(),
             matrix.min(axis=1).tolist(),
             matrix.min().tolist(),
         ],
-        "Sum": [
+        "sum": [
             matrix.sum(axis=0).tolist(),
             matrix.sum(axis=1).tolist(),
             matrix.sum().tolist(),
         ],
     }
+
     return calculations
-print(calculate([1, 2, 3, 4, 5, 6, 7, 8, 9]))
